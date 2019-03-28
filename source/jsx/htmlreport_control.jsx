51427d7a00941115608fccf4ab9728a8880040d3
Norman Pellet <norman.pellet@gmail.com>
2019-03-28 15:12:23 +0100

    ESLint cleanup

--- source/jsx/htmlreport_control.jsx
+++ source/jsx/htmlreport_control.jsx
@@ -1,209 +1,235 @@
-import React from 'react';
+import React from "react";
 import { query as influxquery } from "../influx";
 import { ipcRenderer } from "electron";
 
-
 const possibleGraphs = {
-	pce: 				{ name: 'pce', 			label: "Power conversion efficiency" },
-	power: 				{ name: 'power', 		label: "Power output" },
-	current: 			{ name: 'current', 		label: "Current output" },
-	voltage: 			{ name: 'voltage', 		label: "Voltage output" },
-	jsc: 				{ name: 'jsc', 			label: "Short circuit current" },
-	voc: 				{ name: 'voc', 			label: "Open circuit voltage" },
-	light: 				{ name: 'light', 		label: "Light intensity" },
-	temperature: 		{ name: 'temperature', 	label: "Temperature" },
-	humidity: 			{ name: 'humidity', 	label: "Humidity" }
+  pce: { name: "pce", label: "Power conversion efficiency" },
+  power: { name: "power", label: "Power output" },
+  current: { name: "current", label: "Current output" },
+  voltage: { name: "voltage", label: "Voltage output" },
+  jsc: { name: "jsc", label: "Short circuit current" },
+  voc: { name: "voc", label: "Open circuit voltage" },
+  light: { name: "light", label: "Light intensity" },
+  temperature: { name: "temperature", label: "Temperature" },
+  humidity: { name: "humidity", label: "Humidity" }
 };
 
 class HTMLReportControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {},
+      comment: ""
+    };
 
-	constructor( props ) {
-		super( props );
-		this.state = {
-			data: {} ,
-			comment: ""
-		};
+    this.close = this.close.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.savePDF = this.savePDF.bind(this);
+    this.printPDF = this.printPDF.bind(this);
+  }
 
-		this.close= this.close.bind( this );
-		this.handleInputChange = this.handleInputChange.bind( this );
-		this.handleSelectChange = this.handleSelectChange.bind( this );
-		this.savePDF = this.savePDF.bind( this );
-		this.printPDF = this.printPDF.bind( this );
-	}
+  validateConfig() {
+    this.props.onValidate(this.state);
+    this.close();
+  }
 
-	validateConfig() {
-		this.props.onValidate( this.state );
-		this.close();
-	}
+  savePDF() {
+    ipcRenderer.send("htmlReport.savePDF", {
+      cellName: this.props.cellInfo.cellName
+    });
+  }
 
-	savePDF() {
-		ipcRenderer.send( "htmlReport.savePDF", { cellName: this.props.cellInfo.cellName } );
-	}
+  printPDF() {
+    ipcRenderer.send("htmlReport.printPDF", {
+      cellName: this.props.cellInfo.cellName
+    });
+  }
 
-	printPDF() {
-		ipcRenderer.send( "htmlReport.printPDF", { cellName: this.props.cellInfo.cellName } );
-	}
+  close() {
+    this.props.onClose();
+  }
 
-	close() {
-		this.props.onClose();
-	}
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    this.setState({ [name]: value });
+  }
 
-	handleInputChange(event) {
-	    const target = event.target;
-	    const value = target.type === 'checkbox' ? target.checked : target.value;
-	    const name = target.name;
-	    this.setState( { [name]: value } );
+  handleSelectChange(event) {
+    var options = event.target.options;
+    var value = [];
+    const name = event.target.name;
 
-	}
+    for (var i = 0, l = options.length; i < l; i++) {
+      if (options[i].selected) {
+        value.push(options[i].value);
+      }
+    }
 
-	handleSelectChange( event ) {
+    this.setState({ [name]: value });
+  }
 
-	  var options = event.target.options;
-	  var value = [];
-	  const name =  event.target.name;
+  componentDidUpdate() {
+    ipcRenderer.send("htmlReport.config", this.state);
+  }
 
-	  for (var i = 0, l = options.length; i < l; i++) {
+  componentDidMount() {
+    this.getTrackData();
+  }
 
-	    if ( options[ i ].selected ) {
-	      value.push(options[i].value);
-	    }
-	  }
+  getTrackData() {
+    var db = this.props.db.db;
 
+    return influxquery(
+      'SELECT time,efficiency FROM "' +
+        encodeURIComponent(this.props.measurementName) +
+        '" ORDER BY time ASC limit 1;SELECT time,efficiency FROM "' +
+        encodeURIComponent(this.props.measurementName) +
+        '" ORDER BY time DESC limit 1;',
+      db,
+      this.props.db
+    ).then(async results => {
+      if (!results[0].series) {
+        throw "No measurement with the name " +
+          encodeURIComponent(this.props.measurementName) +
+          " or no associated data";
+      }
 
-	  this.setState( { [ name ]: value } );
-	}
+      let timefrom = results[0].series[0].values[0][0],
+        timeto = results[1].series[0].values[0][0],
+        timeDifference = (new Date(timeto) - new Date(timefrom)) / 1000,
+        grouping = Math.max(1, Math.round(timeDifference / 1000));
 
-	componentDidUpdate() {
-	    ipcRenderer.send( "htmlReport.config", this.state );
-	}
+      influxquery(
+        `SELECT time,iv from "${encodeURIComponent(
+          this.props.measurementName
+        )}_iv" ORDER BY time ASC;`,
+        db,
+        this.props.db
+      ).then(results => {
+        if (!results[0].series) {
+          console.warn("No IV curves for this serie");
+          this.state.data.jv_available = [];
+          this.setState({ data: this.state.data });
+          return;
+        }
 
-	componentDidMount() {
-		this.getTrackData();
-	}
+        let values = results[0].series[0].values;
+        this.state.data.jv_available = values.map((value, index) => {
+          return {
+            ellapsed:
+              Math.round(
+                ((new Date(value[0]) - new Date(timefrom)) / 3600 / 1000) * 10
+              ) / 10,
+            time: value[0]
+          };
+        });
 
-	getTrackData( ) {
+        this.setState({ data: this.state.data });
+      });
+    });
+  }
 
-		var db = this.props.db.db;
+  render() {
+    // test6_1494506615016_iv
+    if (!this.state.data.jv_available) {
+      return null;
+    }
 
-		return influxquery("SELECT time,efficiency FROM \"" + encodeURIComponent( this.props.measurementName ) + "\" ORDER BY time ASC limit 1;SELECT time,efficiency FROM \"" + encodeURIComponent( this.props.measurementName ) + "\" ORDER BY time DESC limit 1;", db, this.props.db ).then( async ( results ) => {
+    const availableGraphs = [];
 
-			if( ! results[ 0 ].series ) {
-				throw "No measurement with the name " + encodeURIComponent( this.props.measurementName ) + " or no associated data";
-			}
+    switch (this.props.cellInfo.trackingMode) {
+      case "MPP":
+        if (this.props.cellInfo.lightMonitoring) {
+          availableGraphs.push(possibleGraphs.pce);
+        }
 
-			let timefrom = results[ 0 ].series[ 0 ].values[ 0 ][ 0 ],
-				timeto = results[ 1 ].series[ 0 ].values[ 0 ][ 0 ],
-				timeDifference = ( new Date( timeto ) - new Date( timefrom ) ) / 1000,
-				grouping = Math.max( 1, Math.round( timeDifference / 1000 ) );
+      case "CONSTV":
+        availableGraphs.push(possibleGraphs.power);
+        availableGraphs.push(possibleGraphs.current);
+        availableGraphs.push(possibleGraphs.voltage);
+        availableGraphs.push(possibleGraphs.jsc);
+        availableGraphs.push(possibleGraphs.voc);
+        break;
 
-			influxquery(`SELECT time,iv from "${ encodeURIComponent( this.props.measurementName ) }_iv" ORDER BY time ASC;`, db, this.props.db ).then( ( results ) => {
+      case "JSC":
+        availableGraphs.push(possibleGraphs.jsc);
+        break;
 
-				if( ! results[ 0 ].series ) {
-					console.warn("No IV curves for this serie");
-					this.state.data.jv_available = [];
-					this.setState( { data: this.state.data } );
-					return;
-				}
+      case "VOC":
+        availableGraphs.push(possibleGraphs.voc);
+        break;
+    }
 
+    if (this.props.cellInfo.lightMonitoring) {
+      availableGraphs.push(possibleGraphs.light);
+    }
 
+    if (this.props.cellInfo.temperatureMonitoring) {
+      availableGraphs.push(possibleGraphs.temperature);
+    }
 
-				let values = results[ 0 ].series[ 0 ].values;
-				this.state.data.jv_available = values.map( ( value, index ) => {
+    if (this.props.cellInfo.humidityMonitoring) {
+      availableGraphs.push(possibleGraphs.humidity);
+    }
 
-					return {
-						ellapsed: Math.round( ( new Date( value[ 0 ] ) - new Date( timefrom ) ) / 3600 / 1000 * 10 ) / 10,
-						time: value[ 0 ]
-					};
-				} );
+    return (
+      <div className="container-fluid">
+        <form className="form-horizontal">
+          <h3>j-V curves</h3>
+          <div className="form-group">
+            <label className="col-sm-3">
+              Select the j-V curves for the report
+            </label>
+            <div className="col-sm-9">
+              <select
+                className="form-control"
+                multiple="multiple"
+                name="jv"
+                value={this.state.jv}
+                onChange={this.handleSelectChange}>
+                {this.state.data.jv_available.map(jv => (
+                  <option key={jv.time} value={jv.time}>
+                    {jv.ellapsed} hours
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
 
-				this.setState( { data: this.state.data } );
-			} );
-		} );
-	}
+          <h3>General</h3>
+          <div className="form-group">
+            <label className="col-sm-3">Comment</label>
+            <div className="col-sm-9">
+              <textarea
+                className="form-control"
+                name="comment"
+                value={this.state.comment}
+                onChange={this.handleInputChange}
+              />
+            </div>
+          </div>
+        </form>
 
-
-
-	render() {
-// test6_1494506615016_iv
-		if( ! this.state.data.jv_available ) {
-			return null;
-		}
-
-		const availableGraphs = [];
-
-		switch( this.props.cellInfo.trackingMode ) {
-
-			case 'MPP':
-
-				if( this.props.cellInfo.lightMonitoring ) {
-					availableGraphs.push( possibleGraphs.pce );
-				}
-
-			case 'CONSTV':
-				availableGraphs.push( possibleGraphs.power );
-				availableGraphs.push( possibleGraphs.current );
-				availableGraphs.push( possibleGraphs.voltage );
-				availableGraphs.push( possibleGraphs.jsc );
-				availableGraphs.push( possibleGraphs.voc );
-			break;
-
-			case 'JSC':
-				availableGraphs.push( possibleGraphs.jsc );
-			break;
-
-			case 'VOC':
-				availableGraphs.push( possibleGraphs.voc );
-			break;
-		}
-
-
-		if( this.props.cellInfo.lightMonitoring ) {
-			availableGraphs.push( possibleGraphs.light );
-		}
-
-		if( this.props.cellInfo.temperatureMonitoring ) {
-			availableGraphs.push( possibleGraphs.temperature );
-		}
-
-		if( this.props.cellInfo.humidityMonitoring ) {
-			availableGraphs.push( possibleGraphs.humidity );
-		}
-
-		return (
-
-			<div className="container-fluid">
-				<form className="form-horizontal">
-
-
-						<h3>j-V curves</h3>
-						<div className="form-group">
-							<label className="col-sm-3">Select the j-V curves for the report</label>
-							<div className="col-sm-9">
-								<select className="form-control" multiple="multiple" name="jv" value={ this.state.jv } onChange={ this.handleSelectChange }>
-									{ this.state.data.jv_available.map( ( jv ) =>
-										<option key={ jv.time } value={ jv.time }>{ jv.ellapsed } hours</option>
-									) }
-								</select>
-							</div>
-						</div>
-
-						<h3>General</h3>
-						<div className="form-group">
-							<label className="col-sm-3">Comment</label>
-							<div className="col-sm-9">
-								<textarea className="form-control" name="comment" value={ this.state.comment } onChange={ this.handleInputChange }>
-								</textarea>
-							</div>
-						</div>
-				</form>
-
-				<div className="btn-group pull-right">
-					<button type="button" className="btn btn-primary" onClick={ this.printPDF }>Print PDF</button>
-					<button type="button" className="btn btn-primary" onClick={ this.savePDF }>Save PDF</button>
-		      	</div>
-			</div>
-		);
-	}
+        <div className="btn-group pull-right">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={this.printPDF}>
+            Print PDF
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={this.savePDF}>
+            Save PDF
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default HTMLReportControl;
