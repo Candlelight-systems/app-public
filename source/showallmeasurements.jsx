612d189116b1c657cb044b666171a982f1927105
Norman Pellet <norman.pellet@gmail.com>
2019-03-28 15:01:21 +0100

    Use URL builder

--- source/showallmeasurements.jsx
+++ source/showallmeasurements.jsx
@@ -1,188 +1,213 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import InfluxForm from "./jsx/influxform.jsx"
-import Error from "./jsx/error.jsx"
+import React from "react";
+import ReactDOM from "react-dom";
+import InfluxForm from "./jsx/influxform.jsx";
+import Error from "./jsx/error.jsx";
 import { query as influxquery } from "./influx";
-import { ipcRenderer, remote } from 'electron';
+import { ipcRenderer, remote } from "electron";
+import { buildTrackerURL } from "../app/util/fetch";
 const dialog = remote.dialog;
 
 let data;
-ipcRenderer.on("loadForm", ( event, d ) => {
+ipcRenderer.on("loadForm", (event, d) => {
+  data = d;
+  render();
+});
 
-	data = d;
-	render();
-} );
-
-function onValidate( formData ) {
-
-	ipcRenderer.send('validateForm', formData );
+function onValidate(formData) {
+  ipcRenderer.send("validateForm", formData);
 }
 
 function onClose() {
-
-	ipcRenderer.send('closeForm');
+  ipcRenderer.send("closeForm");
 }
 
-function pad( number ) {
-
-	if( number < 10 ) {
-		return "0" + number;
-	}
-	return number;
+function pad(number) {
+  if (number < 10) {
+    return "0" + number;
+  }
+  return number;
 }
 
-function downloadData( measurementName ) {
-
-	ipcRenderer.send( "downloadData", data.config, measurementName, undefined, data.instrumentId );
+function downloadData(measurementName) {
+  ipcRenderer.send(
+    "downloadData",
+    data.config,
+    measurementName,
+    undefined,
+    data.instrumentId
+  );
 }
 
+function removeData(measurementName) {
+  dialog.showMessageBox(
+    {
+      type: "question",
+      message: "Are you sure that you want to delete this measurement ?",
+      cancelId: 0,
+      defaultId: 0,
+      title: "Delete this measurement ?",
+      buttons: ["Cancel", "Yes"]
+    },
+    async index => {
+      if (index == 1) {
+        try {
+          await fetch(
+            buildTrackerURL(data.config, "dropMeasurement") +
+              `?measurementName=${measurementName}`
+          );
+          render();
+        } catch (e) {
+          dialog.showMessageBox({
+            type: "error",
+            message: `Error in removing the measurement. The error was :${e.toString()}. Make sure that the database and the server can be accessed.`,
+            cancelId: 0,
+            defaultId: 0,
+            title: "Error",
+            buttons: ["Ok"]
+          });
+        }
 
-function removeData( measurementName ) {
-
-  dialog.showMessageBox( {
-    type: 'question',
-    message: 'Are you sure that you want to delete this measurement ?',
-    cancelId: 0,
-    defaultId: 0,
-    title: "Delete this measurement ?",
-    buttons: [ "Cancel", "Yes" ]
-  }, async ( index ) => {
-
-    if( index == 1 ) {
-
-    	try {
-
-    		await fetch(`http://${ data.config.trackerHost }:${ data.config.trackerPort }/dropMeasurement?measurementName=${measurementName}`)
-    		render();
-
-    	} catch ( e ) {
-
-    		dialog.showMessageBox( {
-			    type: 'error',
-			    message: `Error in removing the measurement. The error was :${ e.toString() }. Make sure that the database and the server can be accessed.`,
-			    cancelId: 0,
-			    defaultId: 0,
-			    title: "Error",
-			    buttons: [ "Ok" ]
-			} );
-    	}
-
-    	try {
-    		await influxquery(`DROP MEASUREMENT ${ measurementName };`, data.configDB.db, data.configDB );
-    	} catch( e ) {
-    		// Error in delete from the DB. However the measurement will be removed from the list even if no data exists
-    	}
+        try {
+          await influxquery(
+            `DROP MEASUREMENT ${measurementName};`,
+            data.configDB.db,
+            data.configDB
+          );
+        } catch (e) {
+          // Error in delete from the DB. However the measurement will be removed from the list even if no data exists
+        }
+      }
     }
-  } );
+  );
 }
 
-
-function formatDate( dateVal ) {
-	let d = new Date( dateVal );
-	return d.getDate() + "/" + ( d.getMonth() + 1 ) + "/" + d.getFullYear() + " " + pad( d.getHours() ) + ":" + pad( d.getMinutes() );
+function formatDate(dateVal) {
+  let d = new Date(dateVal);
+  return (
+    d.getDate() +
+    "/" +
+    (d.getMonth() + 1) +
+    "/" +
+    d.getFullYear() +
+    " " +
+    pad(d.getHours()) +
+    ":" +
+    pad(d.getMinutes())
+  );
 }
 
-async function render( ) {
+async function render() {
+  try {
+    if (!data.config) {
+      throw new Error(
+        `The tracker host is not defined. Open an instrument first before selecting this menu.`
+      );
+    }
 
-	try {
+    let json = await fetch(buildTrackerURL(data.config, "getAllMeasurements"), {
+      method: "GET"
+    })
+      .then(response => response.json())
+      .catch(() => {
+        throw new Error(
+          `Error while connecting to the instrument. Check that you are online and that the instrument is available on your network.`
+        );
+      });
 
-		if( ! data.config ) {
-			throw new Error(`The tracker host is not defined. Open an instrument first before selecting this menu.`);
-		}
+    let jsonArray = [];
+    for (var i in json) {
+      jsonArray.push({
+        measurementName: i,
+        startDate: json[i].startDate,
+        endDate: json[i].endDate,
+        cellInfo: json[i].cellInfo
+      });
+    }
 
-		let json = await fetch( "http://" + data.config.trackerHost + ":" + data.config.trackerPort + "/getAllMeasurements", {
-			method: 'GET'
-		})
-		.then( ( response ) => response.json() )
-		.catch( () => {
-			throw new Error(`Error while connecting to the instrument. Check that you are online and that the instrument is available on your network.`);
-		});
+    jsonArray.sort((a, b) => {
+      return a.startDate - b.startDate;
+    });
 
-		let jsonArray = [];
-		for( var i in json ) {
-			jsonArray.push( {
-				measurementName: i,
-				startDate: json[ i ].startDate,
-				endDate: json[ i ].endDate,
-				cellInfo: json[ i ].cellInfo } );
-		}
+    let tracking;
 
-		jsonArray.sort( ( a, b ) => {
+    ReactDOM.render(
+      <div className="container-fluid">
+        <ul className="list-group">
+          <h2>Showing all existing measurements</h2>
 
-			return a.startDate - b.startDate;
-		} );
+          {jsonArray.map(val => {
+            switch (val.cellInfo.trackingMode) {
+              case "CONSTV":
+                tracking = `Constant voltage`;
+                break;
 
-		let tracking;
+              case "JSC":
+                tracking = `Short circuit current`;
+                break;
 
-		ReactDOM.render(
-			<div className="container-fluid">
-				<ul className="list-group">
-				<h2>
-					Showing all existing measurements
-				</h2>
+              case "VOC":
+                tracking = `Open circuit voltage`;
+                break;
 
-				{ jsonArray.map( ( val ) => {
+              case "MPP":
+                tracking = "Maximum power point";
+                break;
 
-					switch( val.cellInfo.trackingMode ) {
+              default:
+                tracking = "N/A";
+                break;
+            }
 
-						case 'CONSTV':
-							tracking = `Constant voltage`;
-						break;
+            return (
+              <li className="list-group-item" key={val.measurementName}>
+                <div className="pull-right">
+                  <button
+                    className="btn btn-sm btn-primary"
+                    onClick={() => downloadData(val.measurementName)}>
+                    Download data
+                  </button>
+                  {val.endDate && (
+                    <button
+                      className="btn btn-sm btn-danger"
+                      onClick={() => removeData(val.measurementName)}>
+                      Delete data
+                    </button>
+                  )}
+                </div>
 
-						case 'JSC':
-							tracking = `Short circuit current`;
-						break;
+                <div>
+                  <strong>{val.cellInfo.cellName}</strong>
+                </div>
 
-						case 'VOC':
-							tracking = `Open circuit voltage`;
-						break;
+                <div>
+                  {formatDate(val.startDate)}{" "}
+                  <span className="glyphicon glypicon-arrow-left" />{" "}
+                  {val.endDate
+                    ? formatDate(val.endDate) +
+                      " (" +
+                      Math.round(
+                        ((val.endDate - val.startDate) / 1000 / 3600) * 10
+                      ) /
+                        10 +
+                      "h)"
+                    : "(Running)"}
+                </div>
 
-						case 'MPP':
-							tracking = 'Maximum power point'
-						break;
-
-						default:
-							tracking = 'N/A';
-						break;
-					}
-
-					return (
-
-						<li className="list-group-item" key={ val.measurementName } >
-
-							<div className="pull-right">
-								<button className="btn btn-sm btn-primary" onClick={ () => downloadData( val.measurementName ) }>Download data</button>
-								{ val.endDate && <button className="btn btn-sm btn-danger" onClick={ () => removeData( val.measurementName ) }>Delete data</button> }
-							</div>
-
-							<div>
-								<strong>{ val.cellInfo.cellName }</strong>
-							</div>
-
-							<div>
-								{ formatDate( val.startDate ) } <span className="glyphicon glypicon-arrow-left"></span> { val.endDate ? formatDate( val.endDate ) + " (" + Math.round( ( val.endDate - val.startDate ) / 1000 / 3600 * 10 ) / 10 + "h)" : '(Running)' }
-							</div>
-
-							<div>
-								Tracking mode: { tracking }
-							</div>
-						</li>
-					);
-				} ) }
-				</ul>
-			</div>,
-			document.getElementById('root')
-		);
-
-
-	} catch( error ) {
-console.log( error );
-		ReactDOM.render(
-			<div>
-
-	          <Error message={ error.props } errorMethods={ [ [ "Try again", render ] ] } />
-	        </div> ,
-			document.getElementById('root')
-		);
-	}
+                <div>Tracking mode: {tracking}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>,
+      document.getElementById("root")
+    );
+  } catch (error) {
+    console.log(error);
+    ReactDOM.render(
+      <div>
+        <Error message={error.props} errorMethods={[["Try again", render]]} />
+      </div>,
+      document.getElementById("root")
+    );
+  }
 }
